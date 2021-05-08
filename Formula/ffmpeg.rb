cf043c25ec53c127759ddc6824d221cc8ec58807
EitanGoldfrad <eitan.goldfrad@gmail.com>
2021-05-08 23:46:05 +0300

    Update ffmpeg.rb

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,7 +1,7 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "https://ffmpeg.org/releases/ffmpeg-4.4.tar.xz"
+  url "https://ffmpeg.org/releases/ffmpeg-4.3.2.tar.xz"
   version "4.4-with-options" # to distinguish from homebrew-core's ffmpeg
   sha256 "06b10a183ce5371f915c6bb15b7b1fffbe046e8275099c96affc29e17645d909"
   license "GPL-2.0-or-later"
